e62cd13f0e8f0552f87005074e1a452e80393c49
apano-on <73277915+apano-on@users.noreply.github.com>
2021-07-09 13:10:26 +0200

    Update create-properties.sh

--- linkedgeodata-docker/lgd-ontop-web/create-properties.sh
+++ linkedgeodata-docker/lgd-ontop-web/create-properties.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-cat <<EOL
-jdbc.url=jdbc:${ONTOP_DB_URL}
-jdbc.driver=org.postgresql.Driver
-#jdbc.driver=com.postgresql.jdbc.driver
-jdbc.user=${ONTOP_DB_USER}
-jdbc.name=${ONTOP_DB_NAME}
-jdbc.password=${ONTOP_DB_PASSWORD}
-EOL
+env | grep -i "osm\|db\|post"
 
-
-
+cat input/lgd_temp.properties | envsubst > input/lgd.properties
